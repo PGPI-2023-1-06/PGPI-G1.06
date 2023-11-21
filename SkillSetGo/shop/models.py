22c03b43b87767fb04dd43f6331f0d99ca54be6b
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-11-21 18:41:13 +0100

    <feature>[homePage and cataloge]: <Home page Home page Home page and catalogeee>
    [optional body]
    
    [optional footer(s)]

---
+++ SkillSetGo/shop/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+    unique=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+        models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
+    
+
+class Professor(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+    unique=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+        models.Index(fields=['name']),
+        ]
+        verbose_name = 'professor'
+        verbose_name_plural = 'professors'
+    def __str__(self):
+        return self.name
+    
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+    unique=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+        models.Index(fields=['name']),
+        ]
+        verbose_name = 'subject'
+        verbose_name_plural = 'subjects'
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor,related_name='products',on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    available = models.BooleanField(default=True)
+    init_dateTime = models.DateTimeField(auto_now=True)
+    finish_dateTime = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+        models.Index(fields=['id', 'slug']),
+        models.Index(fields=['name']),
+        models.Index(fields=['-created']),
+        ]
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',args=[self.id, self.slug])
