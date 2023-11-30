3d1356adc459ff8449c282d363ecd6205cff390b
crigarher <80218587+crigarher@users.noreply.github.com>
2023-11-30 15:24:01 +0100

    Revert "Feature/deploy"

--- SkillSetGo/account/views.py
+++ SkillSetGo/account/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+
+from shop.models import Comment, Customer, Order
 from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
@@ -25,7 +27,19 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+    # necessary for the shopping cart digit
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, completed=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'cartItems': cartItems})
 
 
 def register(request):
@@ -39,7 +53,28 @@ def register(request):
             user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            # Create a customer object to link orders to users
+            customer = Customer.objects.create(user=new_user, name=new_user.username, email=new_user.email)
+            customer.save()
             return render(request, 'account/register_done.html',{'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request,'account/register.html',{'user_form': user_form})
+
+
+#gestion de reclamaciones
+def reclamations_list(request):
+    reclamations = Comment.objects.filter(reclamation=True)
+
+        
+    return render(request,
+    'account/administration/reclamation.html',
+    {'reclamations': reclamations})
+
+#Cierre de reclamaciones
+def close_reclamation(request,id):
+    Comment.objects.filter(id=id).update(reclamation=False)
+        
+    return render(request,
+    'account/dashboard.html',
+    {'section': 'dashboard'})
